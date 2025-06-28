39551344f7209e827a0dca335157283f7304ec76
arjun-sanjay <arjunssnvp.ixa@gmail.com>
2025-06-28 09:25:52 +0530

    Update mongodbconfig.js

--- mongodbconfig.js
+++ mongodbconfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 function connectToDatabase() {
-  mongoose.connect('mongodb+srv://ramudaynair:Aspire5315@cluster0.xxipblf.mongodb.net/form')
+  mongoose.connect('mongodb+srv://arjun-sanjay:1YmZ2Cs1Y2EYx6ku@cluster0.xxipblf.mongodb.net/form')
     .then(() => {
       console.log('Connected to MongoDB'); 
     })
